ac0e37abb15063b4c2559687c4b853f092c23d74
Ernane Sousa <ernane.computacao@gmail.com>
2023-10-13 13:31:15 -0300

    Adicionando Service e estruturas de métodos

---
+++ ProdutoService.cs
@@ -0,0 +1,37 @@
+using System;
+
+class ProdutoService
+{
+
+    public static void Adicionar(List<Produto> produtos)
+    {
+    }
+
+    public static void Atualizar(List<Produto> produtos)
+    {
+    }
+
+    public static void Buscar(List<Produto> produtos)
+    {
+    }
+
+    public static void CalcularTotalEstoque(List<Produto> produtos)
+    {
+    }
+
+    public static void RelatorioValidadeProdutos(List<Produto> produtos)
+    {
+    }
+
+    public static Produto ReceberDadosProduto(Produto produto)
+    {
+    }
+
+    public static void ImprimirListaDeProdutos(List<Produto> produtos)
+    {
+    }
+
+    public static void ImprimirProduto(Produto produto)
+    {
+    }
+}
