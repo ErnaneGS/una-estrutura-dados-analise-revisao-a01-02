2305fa27b77eca52ab988fdd1e282d719f304b96
Ernane Sousa <ernane.computacao@gmail.com>
2023-10-13 13:28:39 -0300

    Adicionando Objeto Produto

---
+++ Produto.cs
@@ -0,0 +1,8 @@
+class Produto
+{
+    public string Nome { get; set; }
+    public string Codigo { get; set; }
+    public int Quantidade { get; set; }
+    public decimal PrecoUnitario { get; set; }
+    public DateTime DataValidade { get; set; }
+}
